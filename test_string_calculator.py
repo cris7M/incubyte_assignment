0b3f2a7a5aac327676144d978f472a03f1ef453a
cris7M <krishnasanjay010@gmail.com>
2025-03-01 17:30:52 +0530

    test: handle two numbers input

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -12,6 +12,10 @@ class TestStringCalculator(unittest.TestCase):
     def test_single_number(self):
         self.assertEqual(add("1"), 1)
 
-
+    # handle multiple numbers
+    def test_two_number(self):
+        self.assertEqual(add("1,2"),3)
+        self.assertEqual(add("10,20"),30)
+        
 if __name__ == "__main__":
     unittest.main()
