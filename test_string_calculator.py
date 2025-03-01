a737d8a8b248b0a6d730f5ab0ecdec6286c5ed2d
cris7M <krishnasanjay010@gmail.com>
2025-03-01 17:13:26 +0530

    Test: Add function returns the number itself for a single input

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -2,8 +2,15 @@ import unittest
 from string_calculator import add
 
 class TestStringCalculator(unittest.TestCase):
+
+    # handle empty string
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
+
+
+    # handle a single number
+    def test_single_number(self):
+        self.assertEqual(add("1"), 1)
 
 
 if __name__ == "__main__":
